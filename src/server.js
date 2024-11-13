08cdc1af180a998595b3a411a62be57eadd36c7f
Andrii D. <and05gt@gmail.com>
2024-11-13 21:34:38 +0100

    Install Express, Pino-pretty and add logic to server.js. Create .env

--- src/server.js
+++ src/server.js
@@ -1,1 +1,36 @@
-export const setupServer = () => {};
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const PORT = 3000; //!
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    //!
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
