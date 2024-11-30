b7cbc528d4d67b0e35d2fa370f0578471c86d9bf
Andrii D. <and05gt@gmail.com>
2024-11-30 22:06:23 +0100

    Add sorting

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,13 +7,19 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
   const contacts = await getAllContacts({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
+
   res.send({
     status: 200,
     message: 'Successfully found contacts!',
---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,39 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortBy = (value) => {
+  if (typeof value !== 'string') {
+    return '_id';
+  }
+
+  const keysOfContacts = ['_id', 'name'];
+
+  if (keysOfContacts.includes(value)) {
+    return value;
+  }
+
+  return '_id';
+};
+
+const parseSortOrder = (value) => {
+  if (typeof value !== 'string') {
+    return SORT_ORDER.ASC;
+  }
+
+  if ([SORT_ORDER.ASC, SORT_ORDER.DESC].includes(value)) {
+    return value;
+  }
+
+  return SORT_ORDER.ASC;
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortBy = parseSortBy(sortBy);
+  const parsedSortOrder = parseSortOrder(sortOrder);
+
+  return {
+    sortBy: parsedSortBy,
+    sortOrder: parsedSortOrder,
+  };
+};
