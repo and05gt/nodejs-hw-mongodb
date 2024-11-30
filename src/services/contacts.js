5e5b52cb2f9334b4516b98b9aa15600b049256ef
Andrii D. <and05gt@gmail.com>
2024-11-30 20:20:16 +0100

    Add pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,21 @@
 import { ContactsCollection } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({ page, perPage }) => {
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(perPage).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, page, perPage);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
