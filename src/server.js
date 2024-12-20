640935fd550466d4751f1e6e93cfc0425fa062e0
Andrii D. <and05gt@gmail.com>
2024-12-20 19:55:44 +0100

    Add swagger, paths - contacts

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,6 +15,7 @@ export const setupServer = () => {
   const app = express();
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(express.json());
   app.use(cors());
