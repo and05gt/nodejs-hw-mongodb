732e9f439207a84a47939971bc6c6ed3d830dbaf
Andrii D. <and05gt@gmail.com>
2024-12-24 23:24:34 +0100

    Fix name of file

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,7 +18,7 @@ import handlebars from 'handlebars';
 import {
   getFullNameFromGoogleTokenPayload,
   validateCode,
-} from '../utils/googleOAuth2.js';
+} from '../utils/google.js';
 
 export const registerUser = async (payload) => {
   const user = await User.findOne({ email: payload.email });
